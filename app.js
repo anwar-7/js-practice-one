890fd476223c276f93eea341fd34e6ef6c4cf4b2
anwar-7 <anwarjewel007@gmail.com>
2022-11-10 14:30:01 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 // 1. Write a JS code to print numbers from 1 to 10
-
 const printNumber = function () {
   for (let i = 1; i <= 10; i++) {
     console.log(i);
   }
 };
+// printNumber();
 
-printNumber();
+// 2. Write a JS code to print a 2D array
+const printArray = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[0].length; j++) {
+      //   console.log(arr[0].length);
+      console.log(arr[i][j]);
+    }
+  }
+};
+let arr = [
+  [11, 22],
+  [33, 44],
+  [55, 66],
+];
+// printArray(arr);
+
+// 3. Write a JS code to print Even numbers in given array
+const printEven = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      console.log(arr[i]);
+    }
+  }
+};
+let arr2 = [13, 23, 12, 45, 22, 48, 66, 100];
+
+printEven(arr2);
